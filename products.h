094a8e1da729dbe4061ba9c77f8ab36b92e8ac2d
Carlos R. Arias <arias@spu.edu>
2018-03-26 21:55:49 -0700

    Products Interface done

--- products.h
+++ products.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <string>
+using std::string;
 
 class Product{
-
+	string _name;
+	double _price;
+	size_t inventory;
+public:
+	Product(const string&, double, size_t = 0);
+	string ToString()const;
+	double GetPrice()const;
 };
